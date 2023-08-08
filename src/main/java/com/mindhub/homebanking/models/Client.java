7755247f33d84a33bb081475ec63e02861a515f4
Mar7inez <mjmserviciostecnicos@gmail.com>
2023-08-08 19:36:40 -0300

    clean code

--- src/main/java/com/mindhub/homebanking/models/Client.java
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -17,52 +17,37 @@ public class Client {
     private String email;
     @OneToMany(mappedBy="client", fetch=FetchType.EAGER)
     Set<Account> accounts = new HashSet<>();
-
     public Client() {
     }
-
     public Client(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
     }
-
     public long getId() {
         return id;
     }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public Set<Account> getAccounts() {
         return accounts;
     }
-
     public void addAccount(Account account) {
         account.setClient(this);
         this.accounts.add(account);
