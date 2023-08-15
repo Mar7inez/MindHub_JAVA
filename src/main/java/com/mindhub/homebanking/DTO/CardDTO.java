ede1a1127b6cef309e2ba203368e18a8e71f2fe3
Mar7inez <mjmserviciostecnicos@gmail.com>
2023-08-15 18:01:18 -0300

    task ok

--- src/main/java/com/mindhub/homebanking/DTO/CardDTO.java
+++ src/main/java/com/mindhub/homebanking/DTO/CardDTO.java
@@ -8,63 +8,44 @@ import java.time.LocalDate;
 
 public class CardDTO {
     private Long id;
-
-    private CardType cardType;
-
-    private CardColor cardColor;
-
-    private String cardHolder;
-
+    private CardType type;
+    private CardColor color;
+    private String holder;
     private String number;
-
     private String cvv;
-
     private LocalDate fromDate;
-
     private LocalDate thruDate;
-
     public CardDTO(Card card){
-
         id = card.getId();
-        cardType = card.getCardType();
-        cardColor = card.getCardColor();
-        cardHolder = card.getCardHolder();
+        type = card.getCardType();
+        color = card.getCardColor();
+        holder = card.getCardHolder();
         number = card.getNumber();
         cvv = card.getCvv();
         fromDate = card.getFromDate();
         thruDate = card.getThruDate();
-
-
     }
-
     public Long getId() {
         return id;
     }
-
-    public CardType getCardType() {
-        return cardType;
+    public CardType getType() {
+        return type;
     }
-
-    public CardColor getCardColor() {
-        return cardColor;
+    public CardColor getColor() {
+        return color;
     }
-
-    public String getCardHolder() {
-        return cardHolder;
+    public String getHolder() {
+        return holder;
     }
-
     public String getNumber() {
         return number;
     }
-
     public String getCvv() {
         return cvv;
     }
-
     public LocalDate getFromDate() {
         return fromDate;
     }
-
     public LocalDate getThruDate() {
         return thruDate;
     }
